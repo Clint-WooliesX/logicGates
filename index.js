834671b147afad17323000d51e292472e15f02fb
Clint Kingston <ckingston1@woolworths.com.au>
2022-04-20 09:04:46 +1000

    multi switches complete

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-let switchState = { switch1: 0 };
+let switchState = [0,0];
 
-function clickSwitch() {
-  if (switchState.switch1) {
-    theLight = document.getElementById("switchHolder");
-    theLight.className = "switch-holder";
-    theSwitch = document.getElementById("switchSlide");
-    theSwitch.className = "switch-slide";
-    invertText = document.getElementById("labels");
-    invertText.className = "labels";
-    switchState.switch1 = 0;
-    console.log(switchState);
+function clickSwitch(event) {
+theLight = document.body.querySelectorAll(".switch-holder");
+theSwitch = document.body.querySelectorAll(".switch-slide");
+invertText = document.body.querySelectorAll(".labels");
+  if (switchState[event]) {
+    theLight[event].className = "switch-holder";
+    theSwitch[event].className = "switch-slide";
+    invertText[event].className = "labels";
+    switchState[event] = 0;
   } else {
-    theLight = document.getElementById("switchHolder");
-    theLight.className += " switch-holder--switch-on";
-    theSwitch = document.getElementById("switchSlide");
-    theSwitch.className += " switch-slide--switch-on";
-    invertText = document.getElementById("labels");
-    invertText.className += " labels--switch-on";
-    switchState.switch1 = 1;
-    console.log(switchState);
+    theLight[event].className += " switch-holder--switch-on";
+    theSwitch[event].className += " switch-slide--switch-on";
+    invertText[event].className += " labels--switch-on";
+    switchState[event] = 1;
   }
+
+for(item in switchState){
+  console.log("Switch:"+item+" = "+item)
+}
 }
